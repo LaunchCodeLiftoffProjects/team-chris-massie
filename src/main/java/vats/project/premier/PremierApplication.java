a49afcaefdc69a377599ab41255e9b8c8a6a7e86
rlfoster2018 <67378382+rlfoster2018@users.noreply.github.com>
2020-11-30 19:39:14 -0600

    Add files via upload

---
+++ src/main/java/vats/project/premier/PremierApplication.java
@@ -0,0 +1,13 @@
+package vats.project.premier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PremierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vats/project/premier/PremierApplicationTests.java
@@ -0,0 +1,13 @@
+package vats.project.premier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PremierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
