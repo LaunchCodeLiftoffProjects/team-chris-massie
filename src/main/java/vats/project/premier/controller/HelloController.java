b9077a9c5f106b9735a00b7fcd862d9505d2712b
anikavi73@gmail.com <anikavi73@gmail.com>
2020-12-02 18:36:10 -0600

    Hello World! code updated

--- src/main/java/vats/project/premier/controller/HelloController.java
+++ src/main/java/vats/project/premier/controller/HelloController.java
@@ -3,17 +3,18 @@ package vats.project.premier.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+@RequestMapping("hello")
 @Controller
 public class HelloController {
 
-    //localhost:8080
+    //localhost:8080/hello
     @GetMapping
     @ResponseBody
     public String hello() {
         return "Hello World!";
     }
 
-    //localhost:8080/hello?name=World
+    //localhost:8080/hello/hello?name=World
     @GetMapping("hello")
     @ResponseBody
     public String helloWithQueryParm(@RequestParam String name) {
@@ -21,13 +22,13 @@ public class HelloController {
     }
 
     //localhost:8080/hello/World
-    @GetMapping("hello/{name}")
+    @GetMapping("{name}")
     @ResponseBody
     public String helloWithPathVariable(@PathVariable String name){
         return "Hello, " + name+ "!";
     }
 
-    //localhost:8080/form
+    //localhost:8080/hello/form
     @GetMapping("form")
     @ResponseBody
     public String helloForm(){
