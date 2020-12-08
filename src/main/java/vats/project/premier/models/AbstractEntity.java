c05d8e740abbb17bf19237733ab9f6deeebab5f1
rbbanner <rlfoster2018@gmail.com>
2020-12-07 19:43:07 -0600

    Updated User class and User Repository 12/7

--- src/main/java/vats/project/premier/models/AbstractEntity.java
+++ src/main/java/vats/project/premier/models/AbstractEntity.java
@@ -15,7 +15,7 @@ public abstract class AbstractEntity {
     private int id;
 
     @NotNull
-    private String name;
+    public String name;
 
     public int getId() {
         return id;
--- src/main/java/vats/project/premier/models/data/UserRepository.java
+++ src/main/java/vats/project/premier/models/data/UserRepository.java
@@ -1,8 +1,10 @@
 package vats.project.premier.models.data;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import vats.project.premier.models.User;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
 
     User findByUsername(String username);
