e998349513bbab71ead7b0925dd9e26dc9bb65ea
Scott Sullentrop <sscalex@yahoo.com>
2020-12-12 12:20:04 -0600

    Add Games page showing up with fields.

--- src/main/java/vats/project/premier/controller/GameController.java
+++ src/main/java/vats/project/premier/controller/GameController.java
@@ -4,11 +4,17 @@ package vats.project.premier.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import vats.project.premier.models.Game;
 import vats.project.premier.models.data.GameRepository;
 
+import javax.validation.Valid;
+
 @Controller
-@RequestMapping("games")
+@RequestMapping("")
 public class GameController {
 
     @Autowired
@@ -17,7 +23,16 @@ public class GameController {
     @RequestMapping("games")
     public String displayGamesForm(Model model) {
         model.addAttribute("Games form", "Games");
-
+        model.addAttribute(new Game());
+        return "games";
+    }
+    @PostMapping("games")
+    public String processGamesForm(@ModelAttribute @Valid Game newGame, Errors errors, Model model){
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Game");
+            return "game";
+        }
+        gameRepository.save(newGame);
         return "games";
     }
 }
