03f80ce3fe8a1800d927786cc84558fe5c9ea19a
Scott Sullentrop <sscalex@yahoo.com>
2020-12-14 19:00:58 -0600

    Added a form method, submit button, and allowed the database to get entry for game/platform.

--- src/main/java/vats/project/premier/controller/GameController.java
+++ src/main/java/vats/project/premier/controller/GameController.java
@@ -5,36 +5,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import vats.project.premier.models.Game;
-import vats.project.premier.models.data.AchievementRepository;
 import vats.project.premier.models.data.GameRepository;
-import vats.project.premier.models.data.ReviewRepository;
 
 import javax.validation.Valid;
 
-@Controller("games")
+@Controller
+@RequestMapping("")
 public class GameController {
 
     @Autowired
     private GameRepository gameRepository;
 
-    @Autowired
-    private ReviewRepository reviewRepository;
-
-    @Autowired
-    private AchievementRepository achievementRepository;
-
-    @GetMapping("")
+    @RequestMapping("games")
     public String displayGamesForm(Model model) {
-        model.addAttribute(new Game());
         model.addAttribute("Games form", "Games");
+        model.addAttribute(new Game());
         return "games";
     }
-
-    @PostMapping("")
+    @PostMapping("games")
     public String processGamesForm(@ModelAttribute @Valid Game newGame, Errors errors, Model model){
         if (errors.hasErrors()) {
             model.addAttribute("title", "Add Game");
