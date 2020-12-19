6f1b2a290068a189cfdaae62e5c9179fa8919316
anikavi73@gmail.com <anikavi73@gmail.com>
2020-12-19 11:35:10 -0600

    Achievement Page update

--- src/main/java/vats/project/premier/controller/AchievementController.java
+++ src/main/java/vats/project/premier/controller/AchievementController.java
@@ -3,18 +3,35 @@ package vats.project.premier.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import vats.project.premier.models.Achievement;
+import vats.project.premier.models.data.AchievementRepository;
+
+import javax.validation.Valid;
 
 @Controller
 public class AchievementController {
 
     @Autowired
-    private AchievementController achievementRepository;
+    private AchievementRepository achievementRepository;
 
     @RequestMapping("achievements")
     public String displayAchievementsForm(Model model) {
         model.addAttribute("Achievements form", "Achievements");
+        model.addAttribute(new Achievement());
+        return "achievements";
+    }
 
+    @PostMapping("achievements")
+    public String processGamesForm(@ModelAttribute @Valid Achievement newAchievement, Errors errors, Model model){
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add an Achievement");
+            return "achievements";
+        }
+        achievementRepository.save(newAchievement);
         return "achievements";
     }
 }
