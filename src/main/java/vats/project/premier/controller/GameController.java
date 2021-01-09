86484479ed12c4777c375222e7f1aad35e7e56c9
Scott Sullentrop <sscalex@yahoo.com>
2021-01-08 22:31:07 -0600

    Added if/else statement to the validation of the game creation.

--- src/main/java/vats/project/premier/controller/GameController.java
+++ src/main/java/vats/project/premier/controller/GameController.java
@@ -4,11 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import vats.project.premier.models.Achievement;
 import vats.project.premier.models.Game;
 import vats.project.premier.models.Review;
@@ -35,37 +31,38 @@ public class GameController {
     @RequestMapping("games")
     public String displayGamesForm(Model model) {
         model.addAttribute("Games form", "Games");
+        model.addAttribute(new Game());
         model.addAttribute("achievements", achievementRepository.findAll() );
         model.addAttribute("games", gameRepository.findAll());
         model.addAttribute("reviews", reviewRepository.findAll());
-        model.addAttribute(new Game());
+
         return "game/games";
     }
     @PostMapping("games")
     public String processGamesForm(@ModelAttribute @Valid Game newGame, Errors errors, Model model,
-                                   @RequestParam List<Integer>  achievements, @RequestParam List<Integer> reviews){
+                                   @RequestParam(required = false) List<Integer>  achievements, @RequestParam(required = false) List<Integer> reviews){
         if (errors.hasErrors()) {
             model.addAttribute("title", "Add Game");
             model.addAttribute("myerrors", errors.toString());
             return "game/games";
         }
-        List<Achievement> achievementObj = (List<Achievement>) achievementRepository.findAllById(achievements);
-        newGame.setAchievements(achievementObj);
 
-        List<Review> reviewObj = (List<Review>)reviewRepository.findAllById(reviews);
-        newGame.setReviews(reviewObj);
-
+        if(achievements == null) {
+            return "redirect:/game/games";
+        } else {
+            List<Achievement> achievementObj = (List<Achievement>) achievementRepository.findAllById(achievements);
+            newGame.setAchievements(achievementObj);
+        } if(reviews == null) {
+            return "redirect:/game/games";
+        } else {
+            List<Review> reviewObj = (List<Review>) reviewRepository.findAllById(reviews);
+            newGame.setReviews(reviewObj);
+        }
         gameRepository.save(newGame);
-        return "redirect:/games";
+        return "redirect:/game/games";
     }
 
-    @GetMapping("create")
-    public String displayCreateEventForm(Model model) {
-        model.addAttribute("title", "Create Game");
-        model.addAttribute(new Game());
-        model.addAttribute("categories", gameRepository.findAll());
-        return "game/create";
-    }
+
 
     @GetMapping("delete")
     public String displayDeleteGameForm(Model model) {
